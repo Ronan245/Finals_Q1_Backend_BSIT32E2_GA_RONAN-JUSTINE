0e1d6334d8abfb9c1146bfba06c897b22535089b
Ronan245 <C1585-23@itmlyceumalabang.onmicrosoft.com>
2026-03-29 16:57:27 +0800

    Added CreatedAt in model and controller (POST)

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -5,5 +5,7 @@
         public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public bool Completed { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
