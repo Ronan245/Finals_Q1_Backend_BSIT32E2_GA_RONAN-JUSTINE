f5c33e3a30dfa3c47ae2a6c8314adb820492cc6a
Ronan245 <C1585-23@itmlyceumalabang.onmicrosoft.com>
2026-03-29 17:19:36 +0800

    Implemented Challenge B: Backend Ledger and Hashing Logic

---
+++ Helpers/HashHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using TodoApi.Models;
+
+namespace TodoApi.Helpers
+{
+    public static class HashHelper
+    {
+        public static string ComputeHash(Todo todo, string previousHash)
+        {
+            var rawData =
+                $"{todo.Id}|{todo.Title}|{todo.Completed}|{todo.CreatedAt:O}|{previousHash}";
+
+            using var sha256 = SHA256.Create();
+            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            return Convert.ToHexString(bytes);
+        }
+    }
+}
